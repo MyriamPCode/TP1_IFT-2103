f17c0088657a5669f23658202f3d9fd5e77c4451
MyriamPCode <143844964+MyriamPCode@users.noreply.github.com>
2024-09-24 15:24:52 -0400

    Applique la gravité

---
+++ Assets/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float gravity = -9.81f;      
+    public float jumpForce = 5f;        
+    public LayerMask groundLayer;        
+
+    private float verticalVelocity = 0f; 
+    private bool isGrounded;             
+
+    void Update()
+    {
+        // Vérifie si le joueur est au sol
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.1f, groundLayer);
+
+        // Appliquer la gravité
+        if (isGrounded)
+        {
+            if (Input.GetButtonDown("Jump")) // La touche espace
+            {
+                verticalVelocity = jumpForce; // Applique la force de saut
+            }
+            else
+            {
+                verticalVelocity = 0; 
+            }
+        }
+        else
+        {
+            // Appliquer la gravité
+            verticalVelocity += gravity * Time.deltaTime; // La gravité continue à s'appliquer
+        }
+
+        // Appliquer le mouvement vertical
+        transform.Translate(new Vector2(0, verticalVelocity * Time.deltaTime));
+
+
+        /*
+        // Réinitialiser la position si le joueur est tombé en dessous d'un certain seuil 
+        if (transform.position.y < -10f)
+        {
+            transform.position = new Vector2(transform.position.x, 0);
+            verticalVelocity = 0;
+        }
+        */
+    }
+}
