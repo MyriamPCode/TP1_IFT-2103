f713a9766c3d74c5b66dce043a07f522a6d29de6
MyriamPCode <143844964+MyriamPCode@users.noreply.github.com>
2024-09-24 16:36:08 -0400

    Ajout d'une plateforme
    Pour permettre de tester les collisions player-sol

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 10f;
     public float gravity = -9.81f;      
     public float jumpForce = 5f;        
     public LayerMask groundLayer;        
 
     private float verticalVelocity = 0f; 
-    private bool isGrounded;             
+    private bool isGrounded;
 
+
+    private void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.Translate(new Vector2(horizontalMovement, 0f));
+    }
     void Update()
     {
         // Vérifie si le joueur est au sol
@@ -46,5 +53,13 @@ public class Player : MonoBehaviour
             verticalVelocity = 0;
         }
         */
+
+        // Appliquer une force normale si le joueur est au sol
+        if (isGrounded && verticalVelocity < 0)
+        {
+            // Simuler une force normale en réajustant la position
+            transform.position = new Vector2(transform.position.x, Mathf.Floor(transform.position.y));
+            verticalVelocity = 0; // Réinitialiser la vitesse verticale pour éviter un rebond
+        }
     }
 }
