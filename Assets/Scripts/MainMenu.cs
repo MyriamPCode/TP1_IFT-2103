d97c0257d3697994ea62a3a9e858f1a41c230d90
MyriamPCode <143844964+MyriamPCode@users.noreply.github.com>
2024-10-29 15:11:38 -0400

    Création d'un menu Settings avec ses boutons
    Ajout d'un bouton Settings dans le menu principal qui ouvre les settings et ajout d'un bouton X pour fermer les settings

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,14 +6,24 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string levelToLoad;
+    public GameObject settingsWindow;
 
     public void StartGame()
     {
         SceneManager.LoadScene(levelToLoad);
     }
-
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SettingsButton()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        settingsWindow.SetActive(false);
     }
 }
