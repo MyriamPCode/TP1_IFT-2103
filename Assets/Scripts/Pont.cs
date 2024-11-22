4380afc092a9752174f94a44d130560a00ede0ed
Meiqi709 <meili@ensc.fr>
2024-11-22 16:21:36 -0500

    fix bug

--- Assets/Scripts/Pont.cs
+++ Assets/Scripts/Pont.cs
@@ -5,16 +5,43 @@ public class DoorTriggerByName : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "Player")
+        if (other.gameObject.name == "Player1")
         {
-            Debug.Log("Player 1 has entered the door. Loading Player 1's scene...");
-            SceneManager.LoadScene("Main Scene"); 
+            Debug.Log("Player 1 has entered the door.");
+            LoadPlayerScene("Player1");
         }
-        
         else if (other.gameObject.name == "Player2")
         {
-            Debug.Log("Player 2 has entered the door. Loading Player 2's scene...");
-            SceneManager.LoadScene("Main Scene"); 
+            Debug.Log("Player 2 has entered the door.");
+            LoadPlayerScene("Player2");
+        }
+    }
+
+    private void LoadPlayerScene(string playerName)
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (currentSceneName == "SecondScene")
+        {
+            if (playerName == "Player1")
+            {
+                SceneManager.LoadScene("Main Scene");
+            }
+            else if (playerName == "Player2")
+            {
+                SceneManager.LoadScene("Main Scene");
+            }
+        }
+        else if (currentSceneName == "Online SecondScene")
+        {
+            if (playerName == "Player1")
+            {
+                SceneManager.LoadScene("Online Main Scene");
+            }
+            else if (playerName == "Player2")
+            {
+                SceneManager.LoadScene("Online Main Scene");
+            }
         }
     }
 }
