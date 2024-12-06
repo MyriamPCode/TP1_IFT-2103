e470d51d490a64d32180f571c7ce1263abbeaefb
MyriamPCode <143844964+MyriamPCode@users.noreply.github.com>
2024-12-05 19:20:27 -0500

    Creation des animations de marche et idle pour chacun des perso

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -7,64 +7,82 @@ using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
-    public Image[] characterImages;  // Tableau de toutes les images des personnages
-    private int selectedCharacterIndex = 0;  // L'index du personnage sélectionné
+    public GameObject[] characters;
+    public Image[] characterImages; 
+    private int selectedCharacterIndex = 0; 
 
-    // Références aux boutons gauche et droit
     public Button leftButton;
     public Button rightButton;
 
     private bool isSwitchingCharacter = false;
 
+    private Animator animator;
+
     void Start()
     {
-        // Charger le personnage sélectionné (si un choix a été sauvegardé)
-        selectedCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0); // Par défaut 0 si aucun choix
+        selectedCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+        animator = characters[selectedCharacterIndex].GetComponent<Animator>();
 
-        // Désactiver toutes les images sauf celle du personnage sélectionné
         UpdateCharacterDisplay();
 
-        // Ajouter les écouteurs pour les boutons
         leftButton.onClick.AddListener(PreviousCharacter);
         rightButton.onClick.AddListener(NextCharacter);
     }
 
-    // Fonction pour aller au personnage suivant (flèche droite)
     public void NextCharacter()
     {
         selectedCharacterIndex++;
         if (selectedCharacterIndex >= characterImages.Length)
         {
-            selectedCharacterIndex = 0; // Revenir au premier personnage si on dépasse
+            selectedCharacterIndex = 0; 
         }
         UpdateCharacterDisplay();
     }
 
-    // Fonction pour aller au personnage précédent (flèche gauche)
     public void PreviousCharacter()
     {
         selectedCharacterIndex--;
         if (selectedCharacterIndex < 0)
         {
-            selectedCharacterIndex = characterImages.Length - 1; // Revenir au dernier personnage si on descend
+            selectedCharacterIndex = characterImages.Length - 1; 
         }
         UpdateCharacterDisplay();
     }
 
-    // Mettre à jour l'affichage du personnage
     void UpdateCharacterDisplay()
     {
-        // Désactiver toutes les images de personnages
+        foreach (GameObject character in characters)
+        {
+            character.SetActive(false);
+        }
+
+        characters[selectedCharacterIndex].SetActive(true);
+
         foreach (Image img in characterImages)
         {
             img.gameObject.SetActive(false);
         }
 
-        // Activer l'image du personnage sélectionné
         characterImages[selectedCharacterIndex].gameObject.SetActive(true);
 
-        // Sauvegarder la sélection du personnage
         PlayerPrefs.SetInt("SelectedCharacter", selectedCharacterIndex);
-        PlayerPrefs.Save(); // Sauvegarder immédiatement
+        PlayerPrefs.Save(); 
+    }
+
+    void SetCharacterAnimatorController(int characterIndex)
+    {
+        // Charger et appliquer l'Animator Controller correspondant au personnage sélectionné
+        switch (characterIndex)
+        {
+            case 0:
+                animator.runtimeAnimatorController = Resources.Load("Character1_Animator") as RuntimeAnimatorController;
+                break;
+            case 1:
+                animator.runtimeAnimatorController = Resources.Load("Character2_Animator") as RuntimeAnimatorController;
+                break;
+            // Ajoutez des cases pour d'autres personnages si nécessaire
+            default:
+                break;
+        }
     }
 }
