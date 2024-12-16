b5c19f109fa0612d9b8d7bbb14fe1870d843fbcc
MyriamPCode <143844964+MyriamPCode@users.noreply.github.com>
2024-12-16 18:28:04 -0500

    Un bouton Reset pour réinitialiser les couleurs du personnage
    Les boutons pour la peau et les vetements ne sont pas encore fonctionnels

--- Assets/Scripts/CharacterCustomization.cs
+++ Assets/Scripts/CharacterCustomization.cs
@@ -16,10 +16,11 @@ public class CharacterCustomization : MonoBehaviour
     {
         // Assurez-vous que l'image a un matériau appliqué avec le shader
         avatarMaterial = avatarImage.material;
+        LoadColors();
         ApplyColors();
     }
 
-    void ApplyColors()
+    public void ApplyColors()
     {
         // Appliquez les couleurs sur le matériau
         avatarMaterial.SetColor("_SkinColor", playerSkinColor);
@@ -27,27 +28,73 @@ public class CharacterCustomization : MonoBehaviour
         avatarMaterial.SetColor("_HairColor", playerHairColor);
     }
 
+    public void ResetColors()
+    {
+        playerSkinColor = Color.white;        // Remet la couleur de peau par défaut (blanc)
+        playerClothesColor = Color.blue;      // Remet la couleur des vêtements par défaut (bleu)
+        playerHairColor = Color.black;        // Remet la couleur des cheveux par défaut (noir)
+
+        ApplyColors();  // Applique les couleurs par défaut au matériau
+    }
+
     public void ChangeSkinColor(Color newSkinColor)
     {
         playerSkinColor = newSkinColor;
         ApplyColors();
+        SaveColors();
     }
 
     public void ChangeClothesColor(Color newClothesColor)
     {
         playerClothesColor = newClothesColor;
         ApplyColors();
+        SaveColors();
     }
 
     public void ChangeHairColor(Color newHairColor)
     {
         playerHairColor = newHairColor;
         ApplyColors();
+        SaveColors();
     }
 
     public void SetSkinColorToRed()
     {
         ChangeSkinColor(Color.red);
+    }
+
+    public void SetClothesColorToGreen()
+    {
+        ChangeClothesColor(Color.green);
+    }
+
+    public void SetHairColorToBlue()
+    {
+        ChangeHairColor(Color.blue);
+    }
+
+    void SaveColors()
+    {
+        PlayerPrefs.SetString("SkinColor", ColorUtility.ToHtmlStringRGBA(playerSkinColor));
+        PlayerPrefs.SetString("ClothesColor", ColorUtility.ToHtmlStringRGBA(playerClothesColor));
+        PlayerPrefs.SetString("HairColor", ColorUtility.ToHtmlStringRGBA(playerHairColor));
+        PlayerPrefs.Save();
+    }
+
+    void LoadColors()
+    {
+        if (PlayerPrefs.HasKey("SkinColor"))
+        {
+            ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("SkinColor"), out playerSkinColor);
+        }
+        if (PlayerPrefs.HasKey("ClothesColor"))
+        {
+            ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("ClothesColor"), out playerClothesColor);
+        }
+        if (PlayerPrefs.HasKey("HairColor"))
+        {
+            ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("HairColor"), out playerHairColor);
+        }
     }
 }
 
