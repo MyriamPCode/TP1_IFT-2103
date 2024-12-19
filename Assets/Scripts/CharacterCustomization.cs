762bd1caf77fa2c0d7c900c97338365950cacfc3
MyriamPCode <143844964+MyriamPCode@users.noreply.github.com>
2024-12-19 10:09:49 -0500

    Peut changer la couleur des jambes du personnage

--- Assets/Scripts/CharacterCustomization.cs
+++ Assets/Scripts/CharacterCustomization.cs
@@ -8,8 +8,13 @@ using UnityEngine.U2D.Animation;
 
 public class CharacterCustomization : MonoBehaviour
 {
+
+    public SpriteResolver legsResolver;
+
     public SpriteLibraryAsset[] skins;
-    
+    public SpriteLibraryAsset[] torsoSkins;
+    public SpriteLibraryAsset[] legsSkins;
+
     public void SetPremierPersonnage()
     {
         ChangeSkin(skins[0]);
@@ -20,9 +25,43 @@ public class CharacterCustomization : MonoBehaviour
         ChangeSkin(skins[1]);
     }
 
+    public void SetTorsoSkin(int index)
+    {
+        ChangeLegs(torsoSkins[1]);
+    }
+
+    public void SetLegsSkin()
+    {
+        ChangeLegs(legsSkins[1]);
+    }
+
     public void ChangeSkin(SpriteLibraryAsset skin)
     {
         GetComponent<SpriteLibrary>().spriteLibraryAsset = skin;
     }
+
+    public void ChangeLegs(SpriteLibraryAsset legsSkins)
+    {
+        GetComponent<SpriteLibrary>().spriteLibraryAsset = legsSkins;
+    }
+
+    public void ChangeTorso(SpriteLibraryAsset torsoSkins)
+    {
+        GetComponent<SpriteLibrary>().spriteLibraryAsset = torsoSkins;
+    }
+
+    /*
+    public void SetLegsSkin(int index)
+    {
+        if (index >= 0 && index < legsSkins.Length)
+        {
+            legsResolver.spriteLibraryAsset = legsSkins[index];
+        }
+        else
+        {
+            Debug.LogWarning("Index de skin de jambes invalide");
+        }
+    }
+    */
 }
 
